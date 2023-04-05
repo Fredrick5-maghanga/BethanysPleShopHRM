1c0470bd7f82360a5e7a59c6360a7bb2f03b4b58
Fredrick5-maghanga <maghangafredrick5@gmail.com>
2023-04-05 08:56:19 +0300

    Add project files.

---
+++ BethanysPleShopHRM/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPleShopHRM
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+       
+      
+        {
+            
+             string firstFriend = "Maria";
+            //string secondFriend = "Sage";
+            //Console.WriteLine($"My friends are" );
+            //int salary = 0;
+            //bool isActive=false;
+            //string name = "fred";
+            //double discount = 34.5;
+            //name = "frd";
+            //string companyName = "Bethany"; 
+            //Console.WriteLine(companyName);
+            //DateTime hireDate= new DateTime(2023, 1, 19);
+            //Console.WriteLine(hireDate);
+            //int age = 23;
+            //bool a = age== 23; 
+            //Console.WriteLine("Age is greater than 23: " + a);
+            //bool b = age > 23;
+            //Console.WriteLine("Age is greater than 23:" + b);
+            //while (true)
+            //{
+               Console.WriteLine(firstFriend);
+            //}
+            //a = 1;
+            //b = 2;
+            //int sum= a+ b;
+            //Console.WriteLine( "the sum is" +sum);
+
+
+        }
+    }
+}
